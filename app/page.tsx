a7a8b417c21e72372d84bf70b9dae37cecae421c
Iman Jalali <i.jalali88@gmail.com>
2025-07-16 14:17:14 +0330

    create my project section

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,16 @@
 import AboutSection from "@/components/AboutSection";
 import HeroSection from "@/components/HeroSection";
 import Navbar from '@/components/Navbar';
+import ProjectsSection from "@/components/ProjectsSection";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-[#121212] container ">
+    <main className="flex min-h-screen flex-col bg-[#121212] p-6">
       <Navbar />
-      <div className="container mx-auto py-4 px-12">
+      <div className="mx-auto py-4 px-12 m-4">
         <HeroSection />
         <AboutSection />
+        <ProjectsSection />
       </div>
     </main>
   );
